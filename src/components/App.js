c9c3def417dcc47d193c809bd13cb082ec27c736
Ayeshashaikh24 <97972259+Ayeshashaikh24@users.noreply.github.com>
2022-03-28 11:42:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,8 @@ import '../styles/App.css';
 import Welcome from './Welcome';
 
 const App = () => {
-  let name = "Suraj";
   return (
-    <Welcome name={name} />
+    <Welcome name="Ayesha" />
   )
 }
 
